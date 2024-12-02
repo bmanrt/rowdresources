bccd8a3b31a8814f534936e256c7947c47296c6b
mayowa <odunoyemayowa@gmail.com>
2024-12-02 22:43:09 +0100

    new

--- app/index.php
+++ app/index.php
@@ -90,7 +90,7 @@ if ($categoriesResult) {
             <div class="overlay"></div>
             <div class="hero-content">
                 <h1 class="animate-text">ReachOut World Day Media Repository</h1>
-                <p class="hero-description">Beyond reaching the whole world, over 7 billion people will be effectively discipled as they're engaged with Rhapsody of Realities in an accountable way.</p>
+                <p class="hero-description">“How beautiful on the mountains are the feet of those who bring good news...” Isaiah 52:7:</p>
                 
                 <div class="cta-buttons">
                     <button class="btn primary" onclick="window.open('https://rhapsodytv.live/', '_blank')">
